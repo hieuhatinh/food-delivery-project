0314418512083677b56d5d52cfc7cd33a668c5c8
Do Duy Khanh <khanhduydo25042003@gmail.com>
2024-04-04 23:00:44 +0700

    first

--- FoodDeliveryApp/App.js
+++ FoodDeliveryApp/App.js
@@ -6,21 +6,8 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Provider } from 'react-redux'
 import store from './src/store'
 
-import Welcome from './src/screens/Welcome'
-import Notification from './src/screens/Notification'
-import PersonalInfo from './src/screens/PersonalInfo'
-import EditInformation from './src/screens/PersonalInfo/EditInformation'
 import SignIn from './src/screens/auth/SignIn'
-import SignUp from './src/screens/auth/SignUp'
-import BottomTabs from './src/screens/Tabs/BottomTabs'
-import AllCategories from './src/screens/AllCategories'
-import Search from './src/screens/Search'
-import ResultByCategory from './src/screens/Search/ResultByCategory'
-import ResultByName from './src/screens/Search/ResultByName'
-import DetailRestaurant from './src/screens/restaurant/DetailRestaurant'
-import OpenRestaurants from './src/screens/restaurant/OpenRestaurants'
-import DetailMeal from './src/screens/DetailMeal'
-
+import Food_Details from './src/screens/Food_Details'
 const Stack = createNativeStackNavigator()
 
 function App() {
@@ -32,56 +19,7 @@ function App() {
                         initialRouteName='Welcome'
                         screenOptions={{ headerShown: false }}
                     >
-                        <Stack.Screen name='Welcome' component={Welcome} />
-                        <Stack.Screen name='SignIn' component={SignIn} />
-                        <Stack.Screen name='SignUp' component={SignUp} />
-                        <Stack.Screen
-                            name='BottomTabs'
-                            component={BottomTabs}
-                        />
-                        {/* Person infomation */}
-                        <Stack.Screen
-                            name='PersonalInfo'
-                            component={PersonalInfo}
-                        />
-                        <Stack.Screen
-                            name='EditInformation'
-                            component={EditInformation}
-                        />
-                        <Stack.Screen
-                            name='Notification'
-                            component={Notification}
-                        />
-                        <Stack.Screen
-                            name='AllCategories'
-                            component={AllCategories}
-                        />
-
-                        {/* restaurant */}
-                        <Stack.Screen
-                            name='OpenRestaurants'
-                            component={OpenRestaurants}
-                        />
-                        <Stack.Screen
-                            name='DetailRestaurant'
-                            component={DetailRestaurant}
-                        />
-                        {/* Search and result */}
-                        <Stack.Screen name='Search' component={Search} />
-                        <Stack.Screen
-                            name='ResultByCategory'
-                            component={ResultByCategory}
-                        />
-                        <Stack.Screen
-                            name='ResultByName'
-                            component={ResultByName}
-                        />
-
-                        {/* meal */}
-                        <Stack.Screen
-                            name='DetailMeal'
-                            component={DetailMeal}
-                        />
+                        <Stack.Screen name='Food_Details' component={Food_Details} />
                     </Stack.Navigator>
                 </NavigationContainer>
             </Provider>
---
+++ FoodDeliveryApp/src/screens/Food_Details.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { View,StyleSheet,Text,Image } from "react-native";
+import BoundaryIcon from '../components/button/BoundaryIcon'
+import Icon from 'react-native-vector-icons/Entypo'
+const Food_Details =() =>{
+    return (
+        <View style= {styles.container}>
+            <View style={styles.boxHeader}>
+                <BoundaryIcon>
+                    <Icon name="chevron-small-left" size={30}></Icon>
+                </BoundaryIcon>
+                <Text style={{fontSize:20}}>Details</Text>
+            </View>
+            <Image source={require('../assets/images/Heading Image.png')}style={{width:397,borderRadius:20,marginTop:30}}></Image>
+            <View style={styles.buttonUttoraCoffeeHouse}>
+                <Image source={require('../assets/images/Ellipse 1295.png')}/>
+                <Text style={{fontSize:14}}>Uttora Coffee House</Text>
+            </View>
+            <Text style={{fontSize:20,fontWeight:'bold'}}>Chicken & Chips</Text>
+            <Text style={{color:'#a0a5ba',width:350}}>Prosciutto e funghi is a pizza variety that is topped with tomato sauce.</Text>
+
+            
+
+        </View>
+
+    )
+
+}
+const styles= StyleSheet.create({
+    container:{
+        flex:1,
+        marginTop:30,
+        left:10,
+        gap:10
+
+    },
+    boxHeader:{
+        flexDirection:'row',
+        alignItems:'center',
+        gap:20
+
+    },
+    buttonUttoraCoffeeHouse:{
+        flexDirection:'row',
+        alignItems:'center',
+        marginTop:10,
+        padding:14,
+        gap:10,
+        width:190,
+        borderWidth:1,
+        borderRadius:20,
+        borderColor:'#e8e8e8'
+    }
+
+})
+export default Food_Details
